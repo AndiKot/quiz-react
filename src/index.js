85f9ad2f4e94e90d4ad6269cdbc1bb6a4dff7c07
Andrii Kotlenko <andrii.kotlenko@gmail.com>
2021-10-23 12:38:31 +0300

    Routing added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
 
+const app = (
+    <Router>
+        <App />
+    </Router>
+)
+
 ReactDOM.render(
-    <App />,
+    app,
     document.getElementById('root')
 );
